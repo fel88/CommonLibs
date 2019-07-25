a32fae9a35acc1a1ad36f9b0f06d8545e7390c03
fel88 <15663687+fel88@users.noreply.github.com>
2019-07-25 09:02:43 +0300

    +giflib

---
+++ GifLib/GifDataBlock.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace GifLib
+{
+    public class GifDataBlock
+    {
+        public long Shift { get; set; }
+        public virtual void Write(Stream ms)
+        {
+
+        }
+    }
+}
